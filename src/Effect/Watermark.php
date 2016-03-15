8216854b5a8e789531d0f35bc265fe2fd253933a
Michael Pavlista <michael@pavlista.cz>
2016-03-15 17:42:44 +0100

    Watermark effect now supports GD extension.

--- src/Effect/Watermark.php
+++ src/Effect/Watermark.php
@@ -56,10 +56,10 @@ class Watermark extends PictureEffect {
         }
 
         $this->watermark = realpath($watermark);
-        $this->position  = $position;
-        $this->space     = $space;
-        $this->opacity   = $opacity;
-        $this->size      = $size;
+        $this->position = $position;
+        $this->space = $space;
+        $this->opacity = $opacity;
+        $this->size = $size;
     }
 
 
@@ -73,20 +73,22 @@ class Watermark extends PictureEffect {
 
         if($picture->isGd()) {
 
-            $this->watermarkGd($resource);
+            $resource = $this->applyWatermarkGD($resource, $picture);
+
+            $picture->setResource($resource);
         }
         else {
 
-            $this->watermarkImagick($resource);
+            $this->applyWatermarkImagick($resource);
         }
     }
 
 
     /**
-     * Adds watermark on the image using the library Imagick
+     * Watermark effect processed by Imagick
      * @param Imagick $image
      */
-    private function watermarkImagick(Imagick $image) {
+    private function applyWatermarkImagick(Imagick $image) {
 
         $watermark = new Imagick($this->watermark);
         $watermark->evaluateImage(Imagick::EVALUATE_MULTIPLY, $this->opacity, Imagick::CHANNEL_ALPHA);
@@ -111,7 +113,7 @@ class Watermark extends PictureEffect {
                         $image->compositeImage($watermark, $watermark->getImageCompose(), $w, $h);
                     }
                 }
-            return;
+                return;
 
             case 'center':
 
@@ -148,9 +150,80 @@ class Watermark extends PictureEffect {
     }
 
 
-    protected function watermarkGd($resource) {
+    /**
+     * Watermark effect processed by gd
+     * @param resource $resource gd image
+     * @param Picture $picture
+     * @return resource gd image
+     */
+    protected function applyWatermarkGD($resource, Picture $picture) {
 
+        // SOURCE IMAGE DIMENSIONS
+        $pictureWidth  = imagesx($resource);
+        $pictureHeight = imagesy($resource);
 
+        // WATERMARK DIMENSIONS
+        $watermarkPicture = new Picture($this->watermark, NULL, Picture::WORKER_GD);
+
+        $opacity = new Opacity($this->opacity);
+        $opacity->apply($watermarkPicture);
+
+        $watermark = $watermarkPicture->getResource($watermarkPicture::WORKER_GD);
+
+        imagealphablending($watermark, TRUE);
+
+        $watermarkWidth  = imagesx($watermark);
+        $watermarkHeight = imagesx($watermark);
+
+        // CALCULATE WATERMARK POSITION
+        switch($this->position) {
+
+            case 'repeat':
+
+                for($w = 0; $w < $pictureWidth; $w += $watermarkWidth + $this->space) {
+
+                    for($h = 0; $h < $pictureHeight; $h += $watermarkHeight + $this->space) {
+
+                        imagecopy($resource, $watermark, $w, $h, 0, 0, $watermarkWidth, $watermarkHeight);
+                    }
+                }
+
+                return $resource;
+
+            case 'center':
+
+                $positionX = ($pictureWidth - $watermarkWidth) / 2 - $this->offsetX;
+                $positionY = ($pictureHeight - $watermarkHeight) / 2 - $this->offsetY;
+                break;
+
+            case 'topRight':
+
+                $positionX = $pictureWidth - $watermarkWidth - $this->offsetX;
+                $positionY = $this->offsetY;
+                break;
+
+            case 'bottomRight':
+
+                $positionX = $pictureWidth  - $watermarkWidth  - $this->offsetX;
+                $positionY = $pictureHeight - $watermarkHeight - $this->offsetY;
+                break;
+
+            case 'bottomLeft':
+
+                $positionX = $this->offsetX;
+                $positionY = $pictureHeight - $watermarkHeight - $this->offsetY;
+                break;
+
+            default:
+
+                $positionX = $this->offsetX;
+                $positionY = $this->offsetY;
+                break;
+        }
+
+        imagecopy($resource, $watermark, $positionX, $positionY, 0, 0, $watermarkWidth, $watermarkHeight);
+
+        return $resource;
     }
 
 }

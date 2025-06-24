75ff445e4f229a2466872153c9ac6d99c57a7274
Tomáš Huda <huda@rtsoft.cz>
2025-06-24 12:05:54 +0200

    zabraneni mkdir vyhazovani vyjimek

--- src/Picture.php
+++ src/Picture.php
@@ -729,7 +729,7 @@ class Picture
         // CREATE DIRECTORY BY FILE PATH
         if(!file_exists($directory = dirname($file)))
         {
-            if (!mkdir($directory, 0777, TRUE) && !is_dir($directory)) {
+            if (!@mkdir($directory, 0777, TRUE) && !is_dir($directory)) {
                 throw new \RuntimeException(sprintf('Directory "%s" was not created', $directory));
             }
         }
